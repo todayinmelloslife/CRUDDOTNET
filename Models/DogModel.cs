fd4c40f50e99f3a34c43630c5c976b0dc3e69509
John Doe <mariamello.ads@gmail.com>
2025-05-01 20:38:02 -0300

    Add Name property to DogModel class for storing dog's name

--- Models/DogModel.cs
+++ Models/DogModel.cs
@@ -3,4 +3,5 @@ namespace Dogs.Models; // Define o namespace como "Dogs.Models"
 public class 
 {
    public Guid Id { get; set; } // Propriedade para armazenar o ID do cão. // Tipo Guid é usado para garantir autenticidade
+   public string Name { get; set; } = string.Empty; // armazena o nome do cão. Inicializa como string vazia.
 }
